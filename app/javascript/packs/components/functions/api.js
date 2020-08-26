08f81c503ce9d03525bb3a6eccc283bd9551acf1
Ilona <m.zakevicius@gmail.com>
2020-08-26 10:44:25 +0300

    Exchange rates

--- app/javascript/packs/components/functions/api.js
+++ app/javascript/packs/components/functions/api.js
@@ -4,38 +4,45 @@ const CORS = 'https://cors-anywhere.herokuapp.com';
 const ENDPOINT = 'https://api-sandbox.coingate.com/v2/orders';
 
 export const submitOrder = async (data) => {
-	const body = new URLSearchParams();
+  const body = new URLSearchParams();
 
-	body.append('price_amount', data.amount);
-	body.append('price_currency', data.currency);
-	body.append('receive_currency', 'EUR');
+  body.append('price_amount', data.amount);
+  body.append('price_currency', data.currency);
+  body.append('receive_currency', 'EUR');
 
-	fetch(`${CORS}/${ENDPOINT}`, {
-		body,
-		method: 'POST',
-		headers: {
-			Authorization: `Token ${token}`,
-			'Content-Type': 'application/x-www-form-urlencoded'
-		}
-	})
-		.then((res) => console.log(res.json()))
-		.then((data) => console.log(data))
-		.catch((err) => console.log(err));
+  const response = await fetch(`${CORS}/${ENDPOINT}`, {
+    body,
+    method: 'POST',
+    headers: {
+      Authorization: `Token ${token}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+  return response.json();
 };
 
 export const submitCheckout = async (order) => {
-	return '';
+  return '';
 };
 
 export const getOrders = async () => {
-	fetch(`${CORS}/${ENDPOINT}?sort=created_at_desc`, {
-		method: 'GET',
-		mode: 'cors',
-		headers: {
-			Authorization: `Token ${token}`
-		}
-	})
-		.then((res) => res.json())
-		.then((data) => console.log(data))
-		.catch((err) => console.log(err));
+  fetch(`${CORS}/${ENDPOINT}?sort=created_at_desc`, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+};
+
+export const getExchangeRates = async () => {
+  const response = await Promise.all(
+    ['BTC', 'ETH', 'LTC'].map((cur) =>
+      fetch(`${CORS}/https://api.coingate.com/v2/rates/merchant/EUR/${cur}`)
+    )
+  );
+  return Promise.all(response.map((r) => r.json()));
 };
